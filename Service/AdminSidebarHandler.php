4ce4699bce082c3f22425b5fd20cf542343526c0
Olivier Delbruyere <olivier.delbruyere@hotmail.com>
2017-08-16 20:02:05 +0200

    Use RouterInterface instead of Router

--- Service/AdminSidebarHandler.php
+++ Service/AdminSidebarHandler.php
@@ -2,46 +2,55 @@
 
 namespace Dywee\CMSBundle\Service;
 
-use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
 
-class AdminSidebarHandler{
-
+class AdminSidebarHandler
+{
+    /** @var RouterInterface */
     private $router;
 
-    public function __construct(Router $router)
+    /**
+     * AdminSidebarHandler constructor.
+     *
+     * @param RouterInterface $router
+     */
+    public function __construct(RouterInterface $router)
     {
         $this->router = $router;
     }
 
+    /**
+     * @return array
+     */
     public function getSideBarMenuElement()
     {
-        $menu = array(
-            'key' => 'cms',
-            'icon' => 'fa fa-files-o',
-            'label' => 'cms.sidebar.label',
-            'children' => array(
-                array(
-                    'icon' => 'fa fa-list-alt',
+        $menu = [
+            'key'      => 'cms',
+            'icon'     => 'fa fa-files-o',
+            'label'    => 'cms.sidebar.label',
+            'children' => [
+                [
+                    'icon'  => 'fa fa-list-alt',
                     'label' => 'cms.sidebar.table',
                     'route' => $this->router->generate('page_table')
-                ),
-                array(
-                    'icon' => 'fa fa-wp-forms',
+                ],
+                [
+                    'icon'  => 'fa fa-wp-forms',
                     'label' => 'cms.sidebar.form',
                     'route' => $this->router->generate('cms_customForm_table')
-                ),
-                array(
-                    'icon' => 'fa fa-plus',
+                ],
+                [
+                    'icon'  => 'fa fa-plus',
                     'label' => 'cms.sidebar.add_page',
                     'route' => $this->router->generate('page_add')
-                ),
-                array(
-                    'icon' => 'fa fa-warning',
+                ],
+                [
+                    'icon'  => 'fa fa-warning',
                     'label' => 'cms.sidebar.alert',
                     'route' => $this->router->generate('cms_alert_table')
-                ),
-            )
-        );
+                ],
+            ]
+        ];
 
         return $menu;
     }
