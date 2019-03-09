a380daf6445f3dcc199adadae2f11a7243cd69ac
olivier <olivier@likebook.me>
2019-03-09 08:48:26 +0100

    Fix Route annotation deprecation

--- Controller/ParentController.php
+++ Controller/ParentController.php
@@ -2,8 +2,6 @@
 
 namespace Dywee\CMSBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 class ParentController extends \Dywee\CoreBundle\Controller\ParentController
 {
     protected $bundleName = 'NotificationBundle';
